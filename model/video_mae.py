2982287dcc2392c2f4f1482e6e46c1e0920e1c73
kiiae99 <kiiae99@gmail.com>
2024-04-02 12:53:58 +0900

    for using video dataset

---
+++ model/video_mae.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import av
+import numpy as np
+from transformers import VideoMAEImageProcessor, VideoMAEModel
+# MCG-NJU/videomae-base
+# MCG-NJU/videomae-small-finetuned-kinetics
+
+class VideoMAE(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.image_processor = VideoMAEImageProcessor.from_pretrained("MCG-NJU/videomae-small-finetuned-kinetics")
+        self.model = VideoMAEModel.from_pretrained("MCG-NJU/videomae-small-finetuned-kinetics")
+
+    def forward(self, x):
+    #    x = self.image_processor(list(x), do_resize=False, do_center_crop=False, data_format=channels_last, return_tensors="pt")
+        device = self.model.parameters().__next__().device
+        outputs = self.model(x.float().to(device))
+        
+        return outputs.last_hidden_state
+    
+    def get_feature_size(self):
+        return 768
+
+
+
+
+
